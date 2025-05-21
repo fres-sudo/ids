85c08a93a2eb0a9974cb567205734734bbb4d77d
fres-sudo <francescocalicchio@hotmail.com>
2025-05-21 12:02:08 +0200

    [IMP] entities: bundle

---
+++ src/main/java/it/unicam/cs/ids/entities/Bundle.java
@@ -0,0 +1,32 @@
+package it.unicam.cs.ids.entities;
+
+import it.unicam.cs.ids.enums.ApprovalStatus;
+import it.unicam.cs.ids.enums.Coordinates;
+import it.unicam.cs.ids.enums.ProductCategory;
+
+import java.util.Date;
+import java.util.List;
+
+public class Bundle {
+    private long id;
+    private String name;
+    private String description;
+    private ProductCategory category;
+    private ApprovalStatus status;
+    private List<Product> products;
+    private double price;
+    private double discountPercentage;
+    private List<String> tags;
+    private int quantity;
+    private String distributorId;
+    private boolean availableForSale;
+    private boolean availableForShipping;
+    private int estimatedDeliveryTime;
+    private double shippingCost;
+    private String returnPolicy;
+    private Coordinates bundleLocation;
+    private Company creator;
+    private Date createdAt;
+    private Date updatedAt;
+    private Date deletedAt;
+}

ec63edb1da9ece90a2d74d607f8914a1e43a6e74
fres-sudo <francescocalicchio@hotmail.com>
2025-05-23 10:34:48 +0200

    [ADD] mappers

---
+++ src/main/java/it/unicam/cs/ids/mappers/DTOMapper.java
@@ -0,0 +1,26 @@
+package it.unicam.cs.ids.mappers;
+
+/**
+ * This interface defines methods for converting between Data Transfer Objects (DTOs) and Entities.
+ * It is a generic interface that can be implemented for different types of DTOs and Entities.
+ *
+ * @param <DTO> the type of the Data Transfer Object
+ * @param <Entity> the type of the Entity
+ */
+public interface DTOMapper<DTO, Entity> {
+    /**
+     * Converts a DTO to an Entity.
+     *
+     * @param dto the DTO to convert
+     * @return the converted Entity
+     */
+    Entity toEntity(DTO dto);
+
+    /**
+     * Converts an Entity to a DTO.
+     *
+     * @param entity the Entity to convert
+     * @return the converted DTO
+     */
+    DTO toDTO(Entity entity);
+}
